ed752617485b28aac204fc5d4c9f36749d29e6c4
zaanposni <me@zaanposni.com>
2022-01-30 14:58:49 +0100

    loading explorer and footer

--- nginx/src/components/Explorer.svelte
+++ nginx/src/components/Explorer.svelte
@@ -10,6 +10,8 @@
 
     const eventDispatcher = createEventDispatcher();
 
+    export let loading: boolean = false;
+
     function viewItem(id: number) {
         console.log("view", id);
     }
@@ -55,66 +57,74 @@
 <Card>
     <Content class="flex flex-col p-2">
       <div class="flex flex-col">
-          {#if $currentDirectory?.items}
-            {#each $currentDirectory?.items as item}
-                {#if item.isDir}
-                    <div class="flex flex-row items-center">
-                        <!-- Icon -->
-                        <IconButton class="material-icons shrink-0" on:click={() => { eventDispatcher("changeDir", item.id); }}>folder</IconButton>
-                        <div class="flex flex-row grow cursor-pointer bg-on-hover rounded-md px-2 py-1 mr-2"
-                             on:click={() => { eventDispatcher("changeDir", item.id); }} title="Foldername: {item.name}">
-                            <!-- Name -->
-                            <div class="truncate">
-                                {item.name}
+            {#if $currentDirectory?.items && !loading}
+                {#each $currentDirectory?.items as item}
+                    {#if item.isDir}
+                        <div class="flex flex-row items-center">
+                            <!-- Icon -->
+                            <IconButton class="material-icons shrink-0" on:click={() => { eventDispatcher("changeDir", item.id); }}>folder</IconButton>
+                            <div class="flex flex-row grow cursor-pointer bg-on-hover rounded-md px-2 py-1 mr-2"
+                                on:click={() => { eventDispatcher("changeDir", item.id); }} title="Foldername: {item.name}">
+                                <!-- Name -->
+                                <div class="truncate">
+                                    {item.name}
+                                </div>
+                                <div class="grow" />
+                                <!-- Metadata -->
+                                <div title="filesize" class="shrink-0 greyed-text mr-2">{filesize(item.size)}</div>
+                                <DateDisplay title="uploaded at" class="shrink-0 greyed-text" date={item.uploadedAt} />
                             </div>
-                            <div class="grow" />
-                            <!-- Metadata -->
-                            <div title="filesize" class="shrink-0 greyed-text mr-2">{filesize(item.size)}</div>
-                            <DateDisplay title="uploaded at" class="shrink-0 greyed-text" date={item.uploadedAt} />
+                            <!-- Action buttons -->
+                            <div class="flex flex-row shrink-0">
+                                <IconButton class="material-icons invisible">share</IconButton>
+                                <IconButton toggle pressed={item.isPublic} class="material-icons px-0" on:click={() => { console.log("restrict", item.id, item.isPublic); }}>
+                                    <Icon class="material-icons" on>lock_open</Icon>
+                                    <Icon class="material-icons">lock</Icon>
+                                </IconButton>
+                                <IconButton class="material-icons px-0" on:click={() => { console.log("delete", item.id) }}>delete</IconButton>
+                            </div>
                         </div>
-                        <!-- Action buttons -->
-                        <div class="flex flex-row shrink-0">
-                            <IconButton class="material-icons invisible">share</IconButton>
-                            <IconButton toggle pressed={item.isPublic} class="material-icons px-0" on:click={() => { console.log("restrict", item.id, item.isPublic); }}>
-                                <Icon class="material-icons" on>lock_open</Icon>
-                                <Icon class="material-icons">lock</Icon>
-                            </IconButton>
-                            <IconButton class="material-icons px-0" on:click={() => { console.log("delete", item.id) }}>delete</IconButton>
+                        <LinearProgress indeterminate closed={!item.loading}/>
+                    {/if}
+                {/each}
+                {#each $currentDirectory?.items as item}
+                    {#if item.isFile}
+                        <div class="flex flex-row items-center grow-0">
+                            <!-- Icon -->
+                            <IconButton class="material-icons shrink-0" on:click={() => { viewItem(item.id); }}>{getIconBasedOnName(item.name)}</IconButton>
+                            <div class="flex flex-row grow cursor-pointer bg-on-hover rounded-md px-2 py-1 mr-2"
+                                on:click={() => { viewItem(item.id); }} title="Filename: {item.name}">
+                                <!-- Name -->
+                                <div class="truncate">
+                                    {item.name}
+                                </div>
+                                <div class="grow" />
+                                <!-- Metadata -->
+                                <div title="filesize" class="shrink-0 greyed-text mr-2">{filesize(item.size)}</div>
+                                <DateDisplay title="uploaded at" class="shrink-0 greyed-text" date={item.uploadedAt} />
+                            </div>
+                            <!-- Action buttons -->
+                            <div class="flex flex-row shrink-0">
+                                <IconButton class="material-icons px-0" on:click={() => { console.log("share", item.id) }}>share</IconButton>
+                                <IconButton toggle pressed={item.isPublic} class="material-icons px-0" on:click={() => { console.log("restrict", item.id, item.isPublic); }}>
+                                    <Icon class="material-icons" on>lock_open</Icon>
+                                    <Icon class="material-icons">lock</Icon>
+                                </IconButton>
+                                <IconButton class="material-icons px-0" on:click={() => { console.log("delete", item.id) }}>delete</IconButton>
+                            </div>
                         </div>
+                        <LinearProgress indeterminate closed={!item.loading}/>
+                    {/if}
+                {/each}
+            {:else}
+                {#if loading}
+                    <LinearProgress indeterminate />
+                {:else}
+                    <div>
+                        <!-- TODO: add dropzone for new files -->
+                        No items found :(
                     </div>
                 {/if}
-            {/each}
-            {#each $currentDirectory?.items as item}
-                {#if item.isFile}
-                    <div class="flex flex-row items-center grow-0">
-                        <!-- Icon -->
-                        <IconButton class="material-icons shrink-0" on:click={() => { viewItem(item.id); }}>{getIconBasedOnName(item.name)}</IconButton>
-                        <div class="flex flex-row grow cursor-pointer bg-on-hover rounded-md px-2 py-1 mr-2"
-                             on:click={() => { viewItem(item.id); }} title="Filename: {item.name}">
-                            <!-- Name -->
-                            <div class="truncate">
-                                {item.name}
-                            </div>
-                            <div class="grow" />
-                            <!-- Metadata -->
-                            <div title="filesize" class="shrink-0 greyed-text mr-2">{filesize(item.size)}</div>
-                            <DateDisplay title="uploaded at" class="shrink-0 greyed-text" date={item.uploadedAt} />
-                        </div>
-                        <!-- Action buttons -->
-                        <div class="flex flex-row shrink-0">
-                            <IconButton class="material-icons px-0" on:click={() => { console.log("share", item.id) }}>share</IconButton>
-                            <IconButton toggle pressed={item.isPublic} class="material-icons px-0" on:click={() => { console.log("restrict", item.id, item.isPublic); }}>
-                                <Icon class="material-icons" on>lock_open</Icon>
-                                <Icon class="material-icons">lock</Icon>
-                            </IconButton>
-                            <IconButton class="material-icons px-0" on:click={() => { console.log("delete", item.id) }}>delete</IconButton>
-                        </div>
-                    </div>
-                    <LinearProgress indeterminate closed={!item.loading}/>
-                {/if}
-            {/each}
-          {:else}
-            No items found :(
           {/if}
       </div>
     </Content>
--- nginx/src/pages/Home.svelte
+++ nginx/src/pages/Home.svelte
@@ -2,33 +2,45 @@
   import Explorer from "../components/Explorer.svelte";
 import Hierarchy from "../components/Hierarchy.svelte";
   import Quicksearch from "../components/Quicksearch.svelte";
-  import { fillDummyData } from "../stores/directory";
+  import { currentDirectory, fillDummyData } from "../stores/directory";
 
   fillDummyData();
+
+  let explorerLoading: boolean = false;
 
   function changeDirectory(id: number) {
     // 0 is the users root directory
     console.log("change directory", id);
+    explorerLoading = true;
+    currentDirectory.update(x => {
+      x.items = [];
+      return x;
+    });
   }
 </script>
 
 <div class="flex flex-col grow items-center w-full h-full">
-  <div class="w-3/5">
-    <!-- Search box -->
-    <div class="card-container mb-4">
-      <Quicksearch />
-    </div>
+    <div class="w-3/5">
+        <!-- Search box -->
+        <div class="card-container mb-4">
+            <Quicksearch />
+        </div>
 
-    <!-- Hierarchy -->
-    <div class="card-container mb-4">
-      <Hierarchy on:changeDir={(event) => { changeDirectory(event.detail)}} />
-    </div>
+        <!-- Hierarchy -->
+        <div class="card-container mb-4">
+            <Hierarchy on:changeDir={(event) => { changeDirectory(event.detail)}} />
+        </div>
 
-    <!-- Explorer -->
-    <div class="card-container">
-      <Explorer on:changeDir={(event) => { changeDirectory(event.detail)}} />
-    </div>
+        <!-- Explorer -->
+        <div class="card-container">
+            <Explorer on:changeDir={(event) => { changeDirectory(event.detail)}} loading={explorerLoading} />
+        </div>
 
-    <!-- TODO: Footer -->
-  </div>
+        <!-- TODO: Footer -->
+        {#if $currentDirectory?.items?.length}
+            <div class="greyed-text pl-2">
+                {$currentDirectory?.items?.length} items
+            </div>
+        {/if}
+    </div>
 </div>
