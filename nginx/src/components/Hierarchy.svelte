eb1ec1e06b1310eaaf1685ad700b0733f42ecdc6
zaanposni <me@zaanposni.com>
2022-01-30 11:00:56 +0100

    added upload button to hierarchy

--- nginx/src/components/Hierarchy.svelte
+++ nginx/src/components/Hierarchy.svelte
@@ -4,36 +4,66 @@
   import { createEventDispatcher } from "svelte";
   import { loggedInUser } from "../stores/authStore";
   import { currentDirectory } from "../stores/directory";
+  import Menu, { MenuComponentDev } from '@smui/menu';
+  import List, { Item, Text, Graphic } from '@smui/list';
+
+  let addMenu: MenuComponentDev;
+
+  let newDirName: string = "";
 
   const eventDispatcher = createEventDispatcher();
+
+  function createNewDir() {
+    console.log("create new dir");
+  }
+
+  function uploadFile() {
+    console.log("upload file");
+  }
 </script>
 
 <Card>
-  <Content class="flex flex-col">
-    {#if $currentDirectory?.hierarchy}
-      <div class="flex flex-row items-center">
-        <IconButton class="material-icons cursor-pointer" on:click={() => { eventDispatcher("changeDir", 0); }}>folder_open</IconButton>
-        <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", 0); }}>
-          <div class="primary font-medium px-2 py-1">{$loggedInUser.name}</div>
-        </div>
-        <span class="font-light px-1">/</span>
-        {#each $currentDirectory?.hierarchy as directory}
-          {#if !directory.isRoot}
-            <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", directory.id); }}>
-              <div class="px-2 py-1">{directory.name}</div>
+  <Content class="flex flex-col p-2">
+    <div class="flex flex-row items-center">
+        {#if $currentDirectory?.hierarchy}
+            <IconButton class="material-icons cursor-pointer" on:click={() => { eventDispatcher("changeDir", 0); }}>folder_open</IconButton>
+            <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", 0); }}>
+            <div class="primary font-medium px-2 py-1">{$loggedInUser.name}</div>
             </div>
             <span class="font-light px-1">/</span>
-          {/if}
-        {/each}
-      </div>
-    {:else}
-      <div class="flex flex-row items-center">
-        <IconButton class="material-icons cursor-pointer" on:click={() => { eventDispatcher("changeDir", 0); }}>folder_open</IconButton>
-        <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", 0); }}>
-          <div class="primary font-medium px-2 py-1">{$loggedInUser.name}</div>
+            {#each $currentDirectory?.hierarchy as directory}
+            {#if !directory.isRoot}
+                <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", directory.id); }}>
+                <div class="px-2 py-1">{directory.name}</div>
+                </div>
+                <span class="font-light px-1">/</span>
+            {/if}
+            {/each}
+        {:else}
+            <IconButton class="material-icons cursor-pointer" on:click={() => { eventDispatcher("changeDir", 0); }}>folder_open</IconButton>
+                <div class="cursor-pointer bg-on-hover rounded-md" on:click={() => { eventDispatcher("changeDir", 0); }}>
+                <div class="primary font-medium px-2 py-1">{$loggedInUser.name}</div>
+                </div>
+            <span class="font-light px-1">/</span>
+        {/if}
+
+        <div class="grow"></div>
+
+        <div class="shrink-0">
+            <IconButton class="material-icons cursor-pointer" on:click={() => addMenu.setOpen(true)}>add</IconButton>
+            <Menu bind:this={addMenu}>
+            <List>
+                <Item on:SMUI:action={createNewDir}>
+                    <Graphic class="material-icons mr-2">create_new_folder</Graphic>
+                    <Text>Directory</Text>
+                </Item>
+                <Item on:SMUI:action={uploadFile}>
+                    <Graphic class="material-icons mr-2">upload_file</Graphic>
+                    <Text>File</Text>
+                </Item>
+            </List>
+            </Menu>
         </div>
-        <span class="font-light px-1">/</span>
-      </div>
-    {/if}
+    </div>
   </Content>
 </Card>
