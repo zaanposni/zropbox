7be5255b19f449cf9253bde143b48c458cf55121
zaanposni <me@zaanposni.com>
2022-02-12 21:51:13 +0100

    reset password and delete user

--- nginx/src/components/UserExplorer.svelte
+++ nginx/src/components/UserExplorer.svelte
@@ -5,12 +5,14 @@
     import httpClient from "../utils/httpClient";
     import IconButton, { Icon } from "@smui/icon-button";
     import LinearProgress from '@smui/linear-progress';
-    import { loggedInUser, loginName } from "../stores/authStore";
+    import { loginName } from "../stores/authStore";
     import { toastError, toastSuccess } from "../utils/toast";
     import { createUserDialog, createUserDialogReturnFunc, showCreateUserDialog } from "../stores/createUserDialog";
     import Fab from '@smui/fab';
-import { useFocus } from "svelte-navigator";
+    import { changePasswordDialog, changePasswordDialogReturnFunc, showChangePasswordDialog } from "../stores/changePasswordDialog";
+import { confirmDialogReturnFunc, showConfirmDialog } from "../stores/confirmDialog";
 
+    let selectedUser: IUser | null = null;
     const allUsers = httpClient<ILoadingContent<IUser[]>>({});
 
     function setItemLoading(id: number, loading: boolean) {
@@ -52,13 +54,33 @@ import { useFocus } from "svelte-navigator";
     }
 
     function deleteUser(user: IUser) {
-        console.log("delete", user);
-        setItemLoading(user.id, true);
+        function deleteConfirmed(e: any) {
+            if (e?.detail?.action === "yes") {
+                setItemLoading(user.id, true);
+                let deleteEntry = httpClient({});
+                deleteEntry.delete(`/users/${user.name}`, {}, () => {
+                    toastSuccess("Successfully deleted");
+                    allUsers.update(x => {
+                        if (x?.content) {
+                            x.content = x.content.filter(y => y.id !== user.id);
+                        }
+                        return x;
+                    });
+                }, (error) => {
+                    toastError("Failed to delete");
+                    setItemLoading(user.id, false);
+                    console.error(error);
+                });
+            }
+        }
+        confirmDialogReturnFunc.set(deleteConfirmed);
+        showConfirmDialog.set(true);
     }
 
     function resetPassword(user: IUser) {
-        console.log("reset", user);
-        setItemLoading(user.id, true);
+        changePasswordDialogReturnFunc.set(changePassword);
+        showChangePasswordDialog.set(true);
+        selectedUser = user;
     }
 
     function openNewUserDialog() {
@@ -82,6 +104,32 @@ import { useFocus } from "svelte-navigator";
                 });
             }, (error) => {
                 toastError("Failed to create");
+                console.error(error);
+            });
+        }
+    }
+
+    function changePassword(e: any) {
+        if (e?.detail?.action === "update") {
+            setItemLoading(selectedUser.id, true);
+            let updateEntry = httpClient({});
+            const data = {
+                username: selectedUser.name,
+                isAdmin: selectedUser.isAdmin,
+                password: $changePasswordDialog
+            };
+            updateEntry.put(`/users/password`, data, () => {
+                toastSuccess("Successfully updated");
+                allUsers.update(x => {
+                    let index = x?.content?.findIndex(y => y.id === selectedUser.id);
+                    if (index !== null && index !== -1) {
+                        x.content[index].loading = false;
+                    }
+                    return x;
+                });
+            }, (error) => {
+                toastError("Failed to update");
+                setItemLoading(selectedUser.id, false);
                 console.error(error);
             });
         }
